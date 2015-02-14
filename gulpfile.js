84f8d9a42a39f729624432f7d8e69a6196fd20de
Alex Ciarlill <alex.ciarlillo@gmail.com>
2015-02-14 16:38:42 -0500

    cleanup gulpfile and browserify/js task

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,13 @@ var sourcemaps = require('gulp-sourcemaps');
 var less = require('gulp-less');
 var reactify = require('reactify');
 
-gulp.task('browserify', function() {
+gulp.task('js', function() {
     var b = browserify();
     b.transform(reactify);
     b.add('./src/scripts/main.js');
     return b.bundle()
         .pipe(source('main.js'))
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('js', function() {
-
-    var bundler = browserify({
-        entires: ['./src/scripts/main.js'],
-        debug: true
-    });
-
-    var bundle = function() {
-        return bundler
-            .bundle()
-            .pipe(source('bundle.js'))
-            .pipe(buffer())
-            .pipe(sourcemaps.init({loadMaps: true}))
-                .pipe(uglify())
-            .pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest('./dist/js'));
-    };
-
-    return bundle();
+        .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('less', function() {
